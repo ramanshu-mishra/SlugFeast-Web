5bdf134a018478162ff134bf29f8fd8051b97125
ramanshu <vigyanavart@gmail.com>
2026-03-16 18:25:21 +0530

    useSellToksns hook created

--- apps/client/hooks/useSellTokens.tsx
+++ apps/client/hooks/useSellTokens.tsx
@@ -2,10 +2,22 @@
 import { useWriteContract } from "wagmi"
 import abi from "@repo/abi/abi"
 
-export function useSellTokens(){
+export function useSellTokens({address, amount, nonce}:{address: `0x${string}`, amount: number, nonce:number}){
     const writeContract = useWriteContract();
+    const contractAdress = process.env.NEXT_ENV_CONTRACT_ADDRESS as `0x${string}`;
 
     function sellTokens(){
-        
+        writeContract.mutate({
+            abi,
+            address: contractAdress,
+            functionName: "sell",
+            args: [
+                address,
+                amount,
+                nonce
+            ]
+        });
     }
+
+    return {sellTokens};
 }
